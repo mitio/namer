acfec42306d43c5d189bd4184e4cef85585695b4
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-06 17:24:40 +0300

    Fix the navigation in the top bar
    Had forgotten the <li> tags...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def top_navigation_urls
+    {
+      'Home'                    => root_path,
+      'New Project'             => new_naming_project_path,
+      'About'                   => 'TODO',
+    }
+  end
 end
