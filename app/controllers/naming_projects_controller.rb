f7a350c6c23757db828e2c85481e4aa8cbbd4ff2
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-06 16:55:21 +0300

    WIP on a basic NamingProjectsController

---
+++ app/controllers/naming_projects_controller.rb
@@ -0,0 +1,33 @@
+class NamingProjectsController < ApplicationController
+  respond_to :html
+  before_filter :load_naming_project, only: [:show, :edit]
+
+  def new
+  end
+
+  def create
+    @naming_project = NamingProject.new params[:naming_project]
+    @naming_project.ip = request.remote_addr
+    @naming_project.save
+
+    respond_with @naming_project
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @naming_project.update_attributes params[:naming_project]
+
+    respond_with @naming_project
+  end
+
+  private
+
+  def load_naming_project
+    @naming_project = NamingProject.by_key params[:id]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Namer::Application.routes.draw do
   root to: 'home#index'
+  resources :naming_projects
 end
